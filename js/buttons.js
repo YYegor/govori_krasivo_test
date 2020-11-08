4b4f25ede22b8b87c1d5c08c0583013b8ffa5e2a
unknown <egor@yeremenko.com>
2020-11-08 22:47:48 +0300

    refactor + progress bar

--- js/buttons.js
+++ js/buttons.js
@@ -2,12 +2,15 @@
   
 window.onload = config_load();
 
+function g_progress_plus(e)
+	{
+		//add more stars to the progress ⭐
+		console.log("Progress +1");
+		$("#gamef").text($("#gamef").text() + "⭐").fadeIn(500);
+	}
 function config_load(e)
 		{
 			// init all: hide all text bubbles
-			
-			//read config and load content from json
-			console.log("loaded");
 			$("#video1_text_div").hide();
 			$("#video1_uaction_div").hide();
 			$("#video1_div").hide();
@@ -19,30 +22,39 @@ function config_load(e)
 			$("#video3_uaction_div").hide();
 			$("#video3_div").hide();
 			$("#video3_text").hide();
+			
+
+			//read config and load content from json
+			
+			var module_duration = 3;
 
 			var mydata = JSON.parse(data);
-			document.getElementById('video1_text').innerHTML  = mydata[0].module1_text;
-			document.getElementById('video1_uaction').innerHTML  = mydata[0].module1_uaction;
-			document.getElementById('video1').src  = "video\\"+mydata[0].module1_video;
+			console.log("config loaded");
 
-			document.getElementById('video2_text').innerHTML  = mydata[1].module2_text;
-			document.getElementById('video2_uaction').innerHTML  = mydata[1].module2_uaction;
-			document.getElementById('video2').src  = "video\\"+mydata[1].module2_video;
+			$("#video1_text").html(mydata[0].module1_text);
+			$("#video1_uaction").html(mydata[0].module1_uaction);
+			$("#video1").attr("src","video\\"+mydata[0].module1_video);
+
+			$("#video2_text").html(mydata[1].module2_text);
+			$("#video2_uaction").html(mydata[1].module2_uaction);
+			$("#video2").attr("src","video\\"+mydata[1].module2_video);
 			
-			document.getElementById('video3_text').innerHTML  = mydata[2].module3_text;
-			document.getElementById('video3_uaction').innerHTML  = mydata[2].module3_uaction;
-			document.getElementById('video3').src  = "video\\"+mydata[2].module3_video;
+			$("#video3_text").html(mydata[2].module3_text);
+			$("#video3_uaction").html(mydata[2].module3_uaction);
+			$("#video3").attr("src", "video\\"+mydata[2].module3_video);
+			
+			// all elements are hidden
 			
 			$("#video1_text_div").delay(5000).show();
 			$("#video1_uaction_div").show();
-			$("#video1_uaction").click(function (e){ $("#video1_div").show(); $('html, body').animate({scrollTop: $("#video1_div").offset().top}, 2000);} );
-			$("#video1").on('ended',function(){$("#video2_text").show(); $("#video2_uaction_div").fadeIn(500);});
+			$("#video1_uaction").click(function (e){ $("#video1_div").show(); $("html, body").animate({scrollTop: $("#video1_div").offset().top}, 2000);} );
+			$("#video1").on("ended",function(){$("#video2_text").show(); $("#video2_uaction_div").fadeIn(500); g_progress_plus(); });
 
-			$("#video2_uaction").click(function (e){ $("#video2_div").show(); $('html, body').animate({scrollTop: $("#video2_div").offset().top}, 2000);} );
-			$("#video2").on('ended',function(){$("#video3_text").show(); $("#video3_uaction_div").fadeIn(500); $('html, body').animate({scrollTop: $("#video3_text").offset().top}, 2000);});			
+			$("#video2_uaction").click(function (e){ $("#video2_div").show(); $("html, body").animate({scrollTop: $("#video2_div").offset().top}, 2000);} );
+			$("#video2").on("ended",function(){$("#video3_text").show(); $("#video3_uaction_div").fadeIn(500); $("html, body").animate({scrollTop: $("#video3_text").offset().top}, 2000); g_progress_plus();});			
 
-			$("#video3_uaction").click(function (e){ $("#video3_div").show(); $('html, body').animate({scrollTop: $("#video3_div").offset().top}, 2000);} );
-			
+			$("#video3_uaction").click(function (e){ $("#video3_div").show(); $("html, body").animate({scrollTop: $("#video3_div").offset().top}, 2000);} );
+			$("#video3").on("ended", g_progress_plus());						
 
 			console.log("config ended");
 
